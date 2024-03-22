ec8d3bc5b12c231249adcd3fa5457ffd447d5a57
Alex Kobylansky <alex.kobylansky@gmail.com>
2024-03-22 21:02:45 +0200

    Rendered categories from array, added useState

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"]
 
 export const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  const handleClickCategory = (index: number) => {
+    setActiveIndex(index)
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((item, index) => {
+          return <li
+            key={item}
+            onClick={() => handleClickCategory(index)}
+            className={activeIndex === index ? "active" : ""}>{item}</li>
+        })}
       </ul>
     </div>
   )
