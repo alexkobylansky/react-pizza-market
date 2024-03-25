1046def35051cb15925c6c79d0683911bd3ddcbb
Alex Kobylansky <alex.kobylansky@gmail.com>
2024-03-25 18:04:32 +0200

    Changed fetching link

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [pizzas, setPizzas] = useState<Pizza[]>([]);
 
   useEffect(() => {
-    fetch("https://file.notion.so/f/f/b3238354-86d5-4ba6-9ad7-eb01112a9acd/36ad4e93-800e-451b-9831-ae6abe1b28ef/pizzas.json?id=e934efcc-4042-481d-9d73-76f227f1696e&table=block&spaceId=b3238354-86d5-4ba6-9ad7-eb01112a9acd&expirationTimestamp=1711137600000&signature=dWtcMEb71dtmAOYJhgrSSQPMjRN3g5hxaZOoKF6CYIU&downloadName=pizzas.json")
+    fetch("https://64523922bce0b0a0f74001e4.mockapi.io/pizzas")
       .then(response => {
         if (response.status === 200) {
           return response.json()
